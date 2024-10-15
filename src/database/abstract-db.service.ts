36b2e75cdc5466aba0ef4806f1b486fa604b6696
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2024-10-15 08:59:06 +0300

    dd

--- src/database/abstract-db.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export abstract class AbstractDbService {
-  abstract connect(): Promise<void>;
-  abstract disconnect(): Promise<void>;
-  abstract insertOne<T>(table: string, data: T): Promise<void>;
-  abstract findOne<T>(table: string, query: any): Promise<T | null>;
-}
